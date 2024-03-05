2f5dec64a46afc386bbac9073acf11414e1bb6ba
patelvaibhavd <patelvaibhav27@gmail.com>
2024-03-05 15:43:41 +0530

    added dashboard with basic details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', loadChildren: () => import('./components/dashboard/dashboard.module').then(mod => mod.DashboardModule) },
   { path: '', loadChildren: () => import('./components/hourly-charge-calculator/hourly-charge-calculator.module').then(mod => mod.HourlyChargeCalculatorModule) },
   { path: '', loadChildren: () => import('./components/time-difference-calculator/time-difference-calculator.module').then(mod => mod.TimeDifferenceCalculatorModule) }
 ];
